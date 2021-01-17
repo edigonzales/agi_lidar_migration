7daa3a16081a232add7109ad750ad5b3c5252fd9
Stefan Ziegler <edi.gonzales@gmail.com>
2021-01-17 11:33:03 +0100

    readme / exception handling

--- README.md
+++ README.md
@@ -1,19 +1,26 @@
 # agi_lidar_migration
 
+## Create ili2h2gis template db
 ```
 java -jar /Users/stefan/apps/ili2h2gis-4.4.5/ili2h2gis-4.4.5.jar --dbfile template_lidar_2D --strokeArcs --defaultSrsCode 2056 --disableValidation --modeldir ".;http://models.geo.admin.ch" --models SO_AGI_Hoehenkurven_2D_Publikation_20210115 --schemaimport
 
 java -jar /Users/stefan/apps/ili2h2gis-4.4.5/ili2h2gis-4.4.5.jar --dbfile template_lidar_3D --strokeArcs --defaultSrsCode 2056 --disableValidation --modeldir ".;http://models.geo.admin.ch" --models SO_AGI_Hoehenkurven_3D_Publikation_20210115 --schemaimport
 ```
 
+## Test import and export
 ```
 java -jar /Users/stefan/apps/ili2h2gis-4.4.5/ili2h2gis-4.4.5.jar --dbfile /Volumes/Samsung_T5/agi_lidar_migration/unzip/25941218_50cm --strokeArcs --defaultSrsCode 2056 --disableValidation --modeldir ".;http://models.geo.admin.ch" --models SO_AGI_Hoehenkurven_Publikation_20210115 --export fubar.xtf
 ```
 
 ```
 java -jar /Users/stefan/apps/ili2gpkg-4.4.5/ili2gpkg-4.4.5.jar --dbfile fubar.gpkg --strokeArcs --defaultSrsCode 2056 --disableValidation --modeldir ".;http://models.geo.admin.ch" --models SO_AGI_Hoehenkurven_2D_Publikation_20210115 --doSchemaImport --import /Volumes/Samsung_T5/agi_lidar_migration/xtf/25941218_50cm.xtf
-
 ```
+
+## Run and log
+```
+./gradlew app:run 2>&1 | tee lidar.log
+```
+
 
 ## Snippets
 
--- app/src/main/groovy/agi/lidar/App.groovy
+++ app/src/main/groovy/agi/lidar/App.groovy
@@ -43,71 +43,77 @@ tiles = ["26061232_50cm"]
 for (String tile : tiles) {
     println "Processing: $tile"
 
-    new ZipFile(Paths.get(DOWNLOAD_FOLDER, tile + ".zip").toFile().getAbsolutePath()).extractAll(TEMP_FOLDER);
+    try {
+        new ZipFile(Paths.get(DOWNLOAD_FOLDER, tile + ".zip").toFile().getAbsolutePath()).extractAll(TEMP_FOLDER);
 
-    // Read features from Shapefile and insert contours into h2gis database.
-    Shapefile contours = new Shapefile(Paths.get(TEMP_FOLDER, "contour2014_" + tile + ".shp").toFile().getAbsolutePath())
-    println "# Features in Contours = ${contours.count}"
+        // Read features from Shapefile and insert contours into h2gis database.
+        Shapefile contours = new Shapefile(Paths.get(TEMP_FOLDER, "contour2014_" + tile + ".shp").toFile().getAbsolutePath())
+        println "# Features in Contours = ${contours.count}"
 
-    def dbFileName = Paths.get(TEMP_FOLDER, tile + ".mv.db").toFile().getAbsolutePath()
-    new File(dbFileName).bytes = new File(TEMPLATE_DB_FILE).bytes
-    dbFileName = dbFileName.reverse().drop(6).reverse()
-    def db = [url:"jdbc:h2:file:$dbFileName", user:'', password:'', driver:'org.h2.Driver']
+        def dbFileName = Paths.get(TEMP_FOLDER, tile + ".mv.db").toFile().getAbsolutePath()
+        new File(dbFileName).bytes = new File(TEMPLATE_DB_FILE).bytes
+        dbFileName = dbFileName.reverse().drop(6).reverse()
+        def db = [url:"jdbc:h2:file:$dbFileName", user:'', password:'', driver:'org.h2.Driver']
 
-    // TODO:
-    // - test with batch
-    // - use transaction
-    Sql.withInstance(db.url, db.user, db.password, db.driver) { sql ->
-        contours.eachFeature { Feature feature ->
-            def elev = feature["elev"]
-            // elev % 2 == 0
-            Geometry geom = feature.geom
-            for (int i=0; i<geom.numGeometries; i++) {
-                def t_id = sql.firstRow("SELECT next value FOR t_ili2db_seq AS t_id").values().getAt(0)
-                LineString line = geom.getGeometryN(i).reducePrecision("fixed", scale: 1000)
+        // TODO:
+        // - test with batch
+        // - use transaction
+        Sql.withInstance(db.url, db.user, db.password, db.driver) { sql ->
+            contours.eachFeature { Feature feature ->
+                def elev = feature["elev"]
+                // elev % 2 == 0
+                Geometry geom = feature.geom
+                for (int i=0; i<geom.numGeometries; i++) {
+                    def t_id = sql.firstRow("SELECT next value FOR t_ili2db_seq AS t_id").values().getAt(0)
+                    LineString line = geom.getGeometryN(i).reducePrecision("fixed", scale: 1000)
 
-                // Z-Koordinate wird beim Erstellen des LineString ignoriert.
-                def coords = line.coordinates.collect() {it ->
+                    // Z-Koordinate wird beim Erstellen des LineString ignoriert.
+                    def coords = line.coordinates.collect() {it ->
                         new Point(it.x, it.y)
                     }
 
-                // Groovy .unique() ist sehr langsam.
-                def cleanedCoords = coords.stream()
-                        .distinct()
-                        .collect(Collectors.toList());
+                    // Groovy .unique() ist sehr langsam.
+                    def cleanedCoords = coords.stream()
+                            .distinct()
+                            .collect(Collectors.toList());
 
-                if (cleanedCoords.size() > 2) {
-                    LineString cleanedLine = new LineString(coords)
-                    //def insertSql = "INSERT INTO hoehenkurve (t_id, kote, geometrie, jahr) VALUES ($t_id, $elev, ST_LineFromText($cleanedLine.wkt), 2014)"
-                    def insertSql = "INSERT INTO hoehenkurve (t_id, kote, geometrie, jahr) VALUES ($t_id, $elev, ST_UpdateZ(ST_LineFromText($cleanedLine.wkt), $elev), 2014)"
-                    sql.execute(insertSql)
+                    if (cleanedCoords.size() > 2) {
+                        LineString cleanedLine = new LineString(coords)
+                        //def insertSql = "INSERT INTO hoehenkurve (t_id, kote, geometrie, jahr) VALUES ($t_id, $elev, ST_LineFromText($cleanedLine.wkt), 2014)"
+                        def insertSql = "INSERT INTO hoehenkurve (t_id, kote, geometrie, jahr) VALUES ($t_id, $elev, ST_UpdateZ(ST_LineFromText($cleanedLine.wkt), $elev), 2014)"
+                        sql.execute(insertSql)
+                    }
                 }
             }
         }
+
+        // Export XTF
+        // TODO:
+        // - ch.ehi.ili2db.base.Ili2dbException -> Ein Fehler soll die Berechnung des tiles abbrechen aber
+        // nicht den ganzen Prozess
+        Config settings = new Config();
+        new H2gisMain().initConfig(settings);
+        settings.setFunction(Config.FC_EXPORT)
+        settings.setModels(MODEL_NAME)
+        settings.setModeldir(Paths.get("..").toFile().getAbsolutePath()+";"+"http://models.geo.admin.ch")
+        settings.setDbfile(new File(dbFileName).getAbsolutePath());
+        settings.setValidation(false);
+        settings.setItfTransferfile(false);
+        settings.setDburl(db.url);
+        def xtfFileName = Paths.get(XTF_FOLDER, tile + ".xtf").toFile().getAbsolutePath()
+        settings.setXtffile(xtfFileName);
+        Ili2db.run(settings, null);
+
+        def xtfZipFileName = Paths.get(XTF_FOLDER, tile + ".zip")
+        new ZipFile(xtfZipFileName.toFile().getAbsolutePath()).addFile(new File(xtfFileName))
+
+        // Remove unnecessary files
+        new File(TEMP_FOLDER).eachFile (FileType.FILES) { file ->
+            if (file.name.contains('contour2014_') || file.name.contains("cm")) file.delete()
+        }
+    } catch (Exception e) {
+        e.printStackTrace()
+        println e.getMessage()
     }
 
-    // Export XTF
-    // TODO:
-    // - ch.ehi.ili2db.base.Ili2dbException -> Ein Fehler soll die Berechnung des tiles abbrechen aber
-    // nicht den ganzen Prozess
-    Config settings = new Config();
-    new H2gisMain().initConfig(settings);
-    settings.setFunction(Config.FC_EXPORT)
-    settings.setModels(MODEL_NAME)
-    settings.setModeldir(Paths.get("..").toFile().getAbsolutePath()+";"+"http://models.geo.admin.ch")
-    settings.setDbfile(new File(dbFileName).getAbsolutePath());
-    settings.setValidation(false);
-    settings.setItfTransferfile(false);
-    settings.setDburl(db.url);
-    def xtfFileName = Paths.get(XTF_FOLDER, tile + ".xtf").toFile().getAbsolutePath()
-    settings.setXtffile(xtfFileName);
-    Ili2db.run(settings, null);
-
-    def xtfZipFileName = Paths.get(XTF_FOLDER, tile + ".zip")
-    new ZipFile(xtfZipFileName.toFile().getAbsolutePath()).addFile(new File(xtfFileName))
-
-    // Remove unnecessary files
-    new File(TEMP_FOLDER).eachFile (FileType.FILES) { file ->
-        if (file.name.contains('contour2014_') || file.name.contains("cm")) file.delete()
-    }
 }
