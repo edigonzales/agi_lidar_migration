6b5cdeab4bc5e361fd696dd62d15d805760d6f82
Stefan Ziegler <edi.gonzales@gmail.com>
2021-01-30 20:51:24 +0100

    intersection with bounds

--- app/src/main/groovy/agi/lidar/gdal/Contour.groovy
+++ app/src/main/groovy/agi/lidar/gdal/Contour.groovy
@@ -1,12 +1,18 @@
 package agi.lidar.gdal
 
 import geoscript.feature.Feature
+import geoscript.feature.Schema
+import geoscript.geom.Bounds
+import geoscript.geom.Geometry
 import geoscript.geom.Point
 import geoscript.layer.Format
+import geoscript.layer.Layer
 import geoscript.layer.MapAlgebra
 import geoscript.layer.Raster
 import geoscript.layer.Shapefile
-
+import geoscript.workspace.GeoPackage
+import geoscript.workspace.Memory
+import geoscript.workspace.Workspace
 import org.gdal.gdal.Dataset
 import org.gdal.gdal.WarpOptions
 import org.gdal.gdal.gdal
@@ -53,162 +59,138 @@ for (Feature feature: tindex.features) {
     println maxX
     println maxY
 
-    if (new File(TEMP_FOLDER, "input.tif").exists()) new File(TEMP_FOLDER, "input.tif").delete()
-    Dataset dataset = gdal.Open(VRT, gdalconstJNI.GA_ReadOnly_get());
-    Dataset[] datasetArray = dataset as Dataset[] // Java: {dataset}
-
-    Vector<String> options = new Vector<>();
-    options.add("-overwrite")
-    options.add("-s_srs")
-    options.add("epsg:2056")
-    options.add("-t_srs")
-    options.add("epsg:2056")
-    options.add("-te")
-    options.add(new Integer(minX - BUFFER).toString())
-    options.add(new Integer(minY - BUFFER).toString())
-    options.add(new Integer(maxX + BUFFER).toString())
-    options.add(new Integer(maxY + BUFFER).toString())
-    options.add("-tr")
-    options.add("0.5")
-    options.add("0.5")
-    options.add("-wo")
-    options.add("NUM_THREADS=ALL_CPUS")
-    options.add("-r")
-    options.add("bilinear");
-    options.add("-co");
-    options.add("TILED=TRUE");
-    gdal.Warp(Paths.get(TEMP_FOLDER, "input0.tif").toFile().getAbsolutePath(), datasetArray, new WarpOptions(options))
-    dataset.delete()
-
+//    if (new File(TEMP_FOLDER, "input.tif").exists()) new File(TEMP_FOLDER, "input.tif").delete()
+//    Dataset dataset = gdal.Open(VRT, gdalconstJNI.GA_ReadOnly_get());
+//    Dataset[] datasetArray = dataset as Dataset[] // Java: {dataset}
+//
+//    Vector<String> options = new Vector<>();
+//    options.add("-overwrite")
+//    options.add("-s_srs")
+//    options.add("epsg:2056")
+//    options.add("-t_srs")
+//    options.add("epsg:2056")
+//    options.add("-te")
+//    options.add(new Integer(minX - BUFFER).toString())
+//    options.add(new Integer(minY - BUFFER).toString())
+//    options.add(new Integer(maxX + BUFFER).toString())
+//    options.add(new Integer(maxY + BUFFER).toString())
+//    options.add("-tr")
+//    options.add("0.5")
+//    options.add("0.5")
+//    options.add("-wo")
+//    options.add("NUM_THREADS=ALL_CPUS")
+//    options.add("-r")
+//    options.add("bilinear");
+//    options.add("-co");
+//    options.add("TILED=TRUE");
+//    gdal.Warp(Paths.get(TEMP_FOLDER, "input.tif").toFile().getAbsolutePath(), datasetArray, new WarpOptions(options))
+//    dataset.delete()
+//
 //    def infile = Paths.get(TEMP_FOLDER, "input.tif").toFile().getAbsolutePath()
 //    def outfile = Paths.get(TEMP_FOLDER, "output.tif").toFile().getAbsolutePath()
-
-    options.clear()
-    options.add("-overwrite")
-    options.add("-s_srs")
-    options.add("epsg:2056")
-    options.add("-t_srs")
-    options.add("epsg:2056")
-    options.add("-r")
-    options.add("cubicspline")
-    options.add("-co");
-    options.add("TILED=TRUE");
-
-//    def src = new File("/tmp/input.tif")
-//    def dst = new File("/tmp/input2.tif")
-//    if (dst.exists()) dst.delete()
-//    dst << src.bytes
-
-//    gdal.Unlink("/tmp/input.tif")
-
-
 //
-////    Dataset srcDataset = gdal.Open(Paths.get(TEMP_FOLDER, "input.tif").toFile().getAbsolutePath(), gdalconstJNI.GA_ReadOnly_get());
-////    Dataset[] srcDatasetArray = srcDataset as Dataset[]
-////    println srcDataset.getRasterXSize()
-
-
-
-    1.times {
-        println it
-        String n = it.toString()
-        String nplus = new Integer(it as int + 1).toString()
-//        String infile = Paths.get(TEMP_FOLDER, "input"+n+".tif").toFile().getAbsolutePath();
-//        String outfile = Paths.get(TEMP_FOLDER, "input"+nplus+".tif").toFile().getAbsolutePath();
+//    5.times {
+//        println it
 //
-//        Dataset dataset2 = gdal.Open(infile);
-//        Dataset[] datasetArray2 = dataset2 as Dataset[]
-//        gdal.Warp(outfile, datasetArray2, new WarpOptions(options))
-//        dataset2.delete()
-
-        File file = new File(Paths.get(TEMP_FOLDER, "input0.tif").toFile().getAbsolutePath())
-        Format format = Format.getFormat(file)
-        Raster raster = format.read()
-        println "Original Raster Bounds = ${raster.bounds}"
-        println "Original Raster Size = ${raster.size[0]}x${raster.size[1]}"
-
-        Point point = raster.getPoint(100,100)
-        println "Geographic location at pixel 1000,1000 is ${point}"
-
-        double elevation = raster.getValue(point)
-        println elevation
-
-        MapAlgebra algebra = new MapAlgebra()
-
-        String script = """
-// Set option to treat locations outside the source image
-// area as null values
-options { outside = null; }
-
-values = [];
-foreach (dy in -1:1) {
-  foreach (dx in -1:1) {
-      values << src[dx, dy];
-  }
-}
-
-dest = mean(values);
-"""
-
-        Raster output = algebra.calculate(script, [src:raster], outputName: "dest")
-        File outFile = new File(Paths.get(TEMP_FOLDER, "input1.tif").toFile().getAbsolutePath())
-        Format outFormat = Format.getFormat(outFile)
-        outFormat.write(output)
-
-        println "transformedRaster Raster Bounds = ${output.bounds}"
-        println "transformedRaster Raster Size = ${output.size[0]}x${output.size[1]}"
-
-        Point transformedPoint = output.getPoint(100,100)
-        println "Geographic location at pixel 1000,1000 is ${transformedPoint}"
-
-        double transformedElevation = output.getValue(transformedPoint)
-        println transformedElevation
-
-//        Raster tempRaster = raster.transform(
-//                scalex: 3, scaley: 1,
-//                shearx: 0.0, sheary: 0.0,
-//                translatex: 5, translatey: 5,
-//                nodata: [255],
-//                interpolation: "BICUBIC"
-//        )
-//        println "tempRaster Raster Size = ${tempRaster.size[0]}x${tempRaster.size[1]}"
-
-//        Raster transformedRaster = tempRaster.transform(
-//                scalex: 0.5, scaley: 0.5,
-//                shearx: 0.0, sheary: 0.0,
-//                translatex: 0, translatey: 0,
-//                nodata: [255],
-//                interpolation: "NEAREST"
-//        )
+//        File file = new File(infile)
+//        Format format = Format.getFormat(file)
+//        Raster raster = format.read()
 //
-//        println "transformedRaster Raster Bounds = ${transformedRaster.bounds}"
-//        println "transformedRaster Raster Size = ${transformedRaster.size[0]}x${transformedRaster.size[1]}"
+//        MapAlgebra algebra = new MapAlgebra()
 //
-//        Point transformedPoint = transformedRaster.getPoint(100,100)
-//        println "Geographic location at pixel 1000,1000 is ${transformedPoint}"
+//        String script = """
+//// Set option to treat locations outside the source image
+//// area as null values
+//options { outside = null; }
 //
-//        double transformedElevation = transformedRaster.getValue(transformedPoint)
-//        println transformedElevation
-
-
-
-//        File outFile = new File(Paths.get(TEMP_FOLDER, "input1.tif").toFile().getAbsolutePath())
+//values = [];
+//foreach (dy in -1:1) {
+//  foreach (dx in -1:1) {
+//      values << src[dx, dy];
+//  }
+//}
+//
+//dest = mean(values);
+//"""
+//
+//        Raster output = algebra.calculate(script, [src:raster], outputName: "dest")
+//        File outFile = new File(outfile)
 //        Format outFormat = Format.getFormat(outFile)
-//        outFormat.write(transformedRaster)
+//        outFormat.write(output)
+//
+////        println "transformedRaster Raster Bounds = ${output.bounds}"
+////        println "transformedRaster Raster Size = ${output.size[0]}x${output.size[1]}"
+////
+////        Point transformedPoint = output.getPoint(100,100)
+////        println "Geographic location at pixel 1000,1000 is ${transformedPoint}"
+////
+////        double transformedElevation = output.getValue(transformedPoint)
+////        println transformedElevation
+//
+//        def src = new File(infile)
+//        def dst = new File(outfile)
+//        if (src.exists()) src.delete()
+//        src << dst.bytes
+//    }
 
-//        File tmpFile = new File(Paths.get(TEMP_FOLDER, "input1_tmp.tif").toFile().getAbsolutePath())
-//        Format tmpFormat = Format.getFormat(tmpFile)
-//        tmpFormat.write(tempRaster)
+    File file = new File(Paths.get(TEMP_FOLDER, "input.tif").toFile().getAbsolutePath())
+    Format format = Format.getFormat(file)
+    Raster contourRaster = format.read()
 
+    int band = 0
+    int interval = 1
+    boolean simplify = false
+    boolean smooth = false
+    Layer contours = contourRaster.contours(band, interval, simplify, smooth)
 
+    Workspace workspace = new Memory()
+    Layer clippedContours = workspace.create(contours.schema);
+    List<Feature> clippedFeatures = []
+
+    Bounds bounds = new Bounds(minX, minY, maxX, maxY, "EPSG:2056")
+    Schema schema = contours.schema
+
+    contours.eachFeature { Feature feat ->
+        Geometry clipped = feat.geom.intersection(bounds.geometry)
+
+        org.locationtech.jts.geom.Geometry g = clipped.g
+
+        Feature f = new Feature([
+            value: feat.get("value"),
+            geom: clipped
+        ], feat.id, schema)
+        clippedFeatures.add(f)
     }
 
+    clippedContours.add(clippedFeatures)
 
-    // Ohne diesen Befehl werden nur die Daten des
-    // ersten GDAL-Prozess auf die Festplatte gespeichert.
-    // Die nachfolgenden Prozesse erzeugen nur 1KB grosse
-    // TIFF.
-    gdal.GDALDestroyDriverManager()
+//    List<Feature> contoursFeatures = contours.features
+//    for(int i=0; i<contoursFeatures.size(); i++) {
+//        Feature f = contoursFeatures[i]
+//        Geometry clipped = f.geom.intersection(bounds.geometry)
+//
+//        contoursFeatures[i].setGeom(clipped)
+//
+////        println bounds.geometry
+////        println clipped.toString()
+////        println feat.geom.toString()
+//
+////        println clipped.equals(feat.geom)
+//
+////        break
+//
+//    }
+////    contours.update()
+
+
+
+    println "hallo welt"
+
+
+    Workspace geopkg = new GeoPackage(Paths.get(TEMP_FOLDER, tile + ".gpkg").toFile())
+    geopkg.add(clippedContours, tile)
+    geopkg.close() //?
+
 
 
 }
