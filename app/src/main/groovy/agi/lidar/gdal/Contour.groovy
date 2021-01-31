586f7b967cefb7f7c33e44b49124a729040bc0d4
edigonzales <edi.gonzales@gmail.com>
2021-01-31 15:55:11 +0100

    use java 8 / try/catch

--- app/src/main/groovy/agi/lidar/gdal/Contour.groovy
+++ app/src/main/groovy/agi/lidar/gdal/Contour.groovy
@@ -48,63 +48,65 @@ Dataset[] datasetArray = vrtDataset as Dataset[] // Java: {dataset}
 Shapefile tindex = new Shapefile(TINDEX)
 
 for (Feature feature: tindex.features) {
-    String location = feature.get("location")
-    String tile = location.reverse().substring(4,17).reverse()
+    try {
+        String location = feature.get("location")
+        String tile = location.reverse().substring(4,17).reverse()
 
-    //if (tile != "26141236_50cm") continue
+        if (Paths.get(RESULT_FOLDER, tile + ".gpkg").toFile().exists()) continue
+        //if (tile != "26141236_50cm") continue
 
-    def geom = feature.geom
-    def env = geom.envelope
+        def geom = feature.geom
+        def env = geom.envelope
 
-    def minX = env.getMinX() as int
-    def minY = env.getMinY() as int
-    def maxX = env.getMaxX() as int
-    def maxY = env.getMaxY() as int
+        def minX = env.getMinX() as int
+        def minY = env.getMinY() as int
+        def maxX = env.getMaxX() as int
+        def maxY = env.getMaxY() as int
 
-    println minX
-    println minY
-    println maxX
-    println maxY
+        println minX
+        println minY
+        println maxX
+        println maxY
 
-    def infile = Paths.get(TEMP_FOLDER, "input.tif").toFile().getAbsolutePath()
-    def outfile = Paths.get(TEMP_FOLDER, "output.tif").toFile().getAbsolutePath()
+        def infile = Paths.get(TEMP_FOLDER, "input.tif").toFile().getAbsolutePath()
+        def outfile = Paths.get(TEMP_FOLDER, "output.tif").toFile().getAbsolutePath()
 
-    if (new File(infile).exists()) new File(infile).delete()
-//    Dataset dataset = gdal.Open(VRT, gdalconstJNI.GA_ReadOnly_get());
-//    Dataset[] datasetArray = dataset as Dataset[] // Java: {dataset}
+        if (new File(infile).exists()) new File(infile).delete()
+    //    Dataset dataset = gdal.Open(VRT, gdalconstJNI.GA_ReadOnly_get());
+    //    Dataset[] datasetArray = dataset as Dataset[] // Java: {dataset}
 
-    Vector<String> options = new Vector<>();
-    options.add("-overwrite")
-    options.add("-s_srs")
-    options.add("epsg:2056")
-    options.add("-t_srs")
-    options.add("epsg:2056")
-    options.add("-te")
-    options.add(new Integer(minX - BUFFER).toString())
-    options.add(new Integer(minY - BUFFER).toString())
-    options.add(new Integer(maxX + BUFFER).toString())
-    options.add(new Integer(maxY + BUFFER).toString())
-    options.add("-tr")
-    options.add("0.5")
-    options.add("0.5")
-    options.add("-wo")
-    options.add("NUM_THREADS=ALL_CPUS")
-    options.add("-r")
-    options.add("bilinear");
-    options.add("-co");
-    options.add("TILED=TRUE");
-    gdal.Warp(Paths.get(infile).toFile().getAbsolutePath(), datasetArray, new WarpOptions(options))
+        Vector<String> options = new Vector<>();
+        options.add("-overwrite")
+        options.add("-s_srs")
+        options.add("epsg:2056")
+        options.add("-t_srs")
+        options.add("epsg:2056")
+        options.add("-te")
+        options.add(new Integer(minX - BUFFER).toString())
+        options.add(new Integer(minY - BUFFER).toString())
+        options.add(new Integer(maxX + BUFFER).toString())
+        options.add(new Integer(maxY + BUFFER).toString())
+        options.add("-tr")
+        options.add("0.5")
+        options.add("0.5")
+        options.add("-wo")
+        options.add("NUM_THREADS=ALL_CPUS")
+        options.add("-r")
+        options.add("bilinear");
+        options.add("-co");
+        options.add("TILED=TRUE");
+        gdal.Warp(Paths.get(infile).toFile().getAbsolutePath(), datasetArray, new WarpOptions(options))
 
-    5.times {
-        println it
+        5.times {
+            println it
 
-        File file = new File(infile)
-        Format format = Format.getFormat(file)
-        Raster raster = format.read()
+            File file = new File(infile)
+            Format format = Format.getFormat(file)
+            Raster raster = format.read()
 
-        MapAlgebra algebra = new MapAlgebra()
+            MapAlgebra algebra = new MapAlgebra()
 
-        String script = """
+            String script = """
 // Set option to treat locations outside the source image
 // area as null values
 options { outside = null; }
@@ -118,76 +120,79 @@ foreach (dy in -1:1) {
 
 dest = mean(values);
 """
-        Raster output = algebra.calculate(script, [src:raster], outputName: "dest")
-        File outFile = new File(outfile)
-        Format outFormat = Format.getFormat(outFile)
-        outFormat.write(output)
+            Raster output = algebra.calculate(script, [src:raster], outputName: "dest")
+            File outFile = new File(outfile)
+            Format outFormat = Format.getFormat(outFile)
+            outFormat.write(output)
 
-        def src = new File(infile)
-        def dst = new File(outfile)
-        if (src.exists()) src.delete()
-        src << dst.bytes
-    }
+            def src = new File(infile)
+            def dst = new File(outfile)
+            if (src.exists()) src.delete()
+            src << dst.bytes
+        }
 
-    File file = new File(Paths.get(infile).toFile().getAbsolutePath())
-    Format format = Format.getFormat(file)
-    Raster contourRaster = format.read()
+        File file = new File(Paths.get(infile).toFile().getAbsolutePath())
+        Format format = Format.getFormat(file)
+        Raster contourRaster = format.read()
 
-    int band = 0
-    def interval = 0.5
-    boolean simplify = false
-    boolean smooth = false
-    Layer contours = contourRaster.contours(band, interval, simplify, smooth)
+        int band = 0
+        def interval = 0.5
+        boolean simplify = false
+        boolean smooth = false
+        Layer contours = contourRaster.contours(band, interval, simplify, smooth)
 
-    Workspace workspace = new Memory()
-    Layer clippedContours = workspace.create(contours.schema);
-    List<Feature> clippedFeatures = []
+        Workspace workspace = new Memory()
+        Layer clippedContours = workspace.create(contours.schema);
+        List<Feature> clippedFeatures = []
 
-    Bounds bounds = new Bounds(minX, minY, maxX, maxY, "EPSG:2056")
-    Schema schema = contours.schema
+        Bounds bounds = new Bounds(minX, minY, maxX, maxY, "EPSG:2056")
+        Schema schema = contours.schema
 
-    contours.eachFeature { Feature feat ->
-        // Scheint ein Bug zu sein: Falls das Intersection-Resultat
-        // ein MultiLineString ist, werden die einzelnen LineStrings
-        // miteinander verbunden.
-        // Ob jetzt die Umwandlung zu JTS und zurück noch notwendig
-        // ist, weiss ich nicht. Jedenfalls das Auseinanderpfrimeln
-        // braucht es.
+        contours.eachFeature { Feature feat ->
+            // Scheint ein Bug zu sein: Falls das Intersection-Resultat
+            // ein MultiLineString ist, werden die einzelnen LineStrings
+            // miteinander verbunden.
+            // Ob jetzt die Umwandlung zu JTS und zurück noch notwendig
+            // ist, weiss ich nicht. Jedenfalls das Auseinanderpfrimeln
+            // braucht es.
 
-        // Ist garantiert, dass ein contour-Geometrie immer
-        // ein LineString ist?
-        org.locationtech.jts.geom.LineString fg = feat.geom.g
-        org.locationtech.jts.geom.Polygon bg = bounds.geometry.g
-        org.locationtech.jts.geom.Geometry cg = OverlayOp.overlayOp(fg, bg, OverlayOp.INTERSECTION)
+            // Ist garantiert, dass ein contour-Geometrie immer
+            // ein LineString ist?
+            org.locationtech.jts.geom.LineString fg = feat.geom.g
+            org.locationtech.jts.geom.Polygon bg = bounds.geometry.g
+            org.locationtech.jts.geom.Geometry cg = OverlayOp.overlayOp(fg, bg, OverlayOp.INTERSECTION)
 
-        if (cg instanceof org.locationtech.jts.geom.MultiLineString) {
-            for (int j=0; j<cg.numGeometries; j++) {
-                org.locationtech.jts.geom.LineString lineString = (org.locationtech.jts.geom.LineString) cg.getGeometryN(j)
+            if (cg instanceof org.locationtech.jts.geom.MultiLineString) {
+                for (int j=0; j<cg.numGeometries; j++) {
+                    org.locationtech.jts.geom.LineString lineString = (org.locationtech.jts.geom.LineString) cg.getGeometryN(j)
+                    def uuid = UUID.randomUUID().toString()
+                    Feature f = new Feature([
+                            value: feat.get("value"),
+                            geom: new LineString(lineString)
+                    ], uuid, schema)
+                    clippedFeatures.add(f)
+                }
+            } else {
                 def uuid = UUID.randomUUID().toString()
                 Feature f = new Feature([
                         value: feat.get("value"),
-                        geom: new LineString(lineString)
+                        geom: new LineString(cg)
                 ], uuid, schema)
                 clippedFeatures.add(f)
             }
-        } else {
-            def uuid = UUID.randomUUID().toString()
-            Feature f = new Feature([
-                    value: feat.get("value"),
-                    geom: new LineString(cg)
-            ], uuid, schema)
-            clippedFeatures.add(f)
         }
+
+        clippedContours.add(clippedFeatures)
+
+        if (Paths.get(RESULT_FOLDER, tile + ".gpkg").toFile().exists()) Paths.get(RESULT_FOLDER, tile + ".gpkg").toFile().delete()
+
+        Workspace geopkg = new GeoPackage(Paths.get(RESULT_FOLDER, tile + ".gpkg").toFile())
+        geopkg.add(clippedContours, tile)
+        geopkg.close()
+    } catch (Exception e) {
+        e.printStackTrace()
+        System.err.println(e.getMessage())
     }
-
-    clippedContours.add(clippedFeatures)
-
-    if (Paths.get(RESULT_FOLDER, tile + ".gpkg").toFile().exists()) Paths.get(RESULT_FOLDER, tile + ".gpkg").toFile().delete()
-
-    Workspace geopkg = new GeoPackage(Paths.get(RESULT_FOLDER, tile + ".gpkg").toFile())
-    geopkg.add(clippedContours, tile)
-    geopkg.close()
-
     println "hallo welt"
 }
 
