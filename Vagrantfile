c5d7d5b7fc2155c05062b9897db12cd4160b99bd
Stefan Ziegler <edi.gonzales@gmail.com>
2021-02-05 21:05:15 +0100

    start with 2018

--- Vagrantfile
+++ Vagrantfile
@@ -50,7 +50,7 @@ Vagrant.configure("2") do |config|
   # argument is a set of non-required options.
   # config.vm.synced_folder "../data", "/vagrant_data"
   #config.vm.synced_folder "/Volumes/Samsung_T5/", "/Volumes/Samsung_T5/"
-  config.vm.synced_folder "/media/stefan/Samsung_T5/", "/Volumes/Samsung_T5/"
+  #config.vm.synced_folder "/media/stefan/Samsung_T5/", "/Volumes/Samsung_T5/"
 
   # Provider-specific configuration so you can fine-tune various
   # backing providers for Vagrant. These expose provider-specific options.
---
+++ app/src/main/groovy/agi/lidar/_2018/Download.groovy
@@ -0,0 +1,19 @@
+package agi.lidar._2018
+
+import java.nio.file.Paths
+
+def DOWNLOAD_FOLDER = "/media/stefan/Samsung_T5/geodata/ch.so.agi.lidar_2014.dtm/"
+def DOWNLOAD_URL = "https://geo.so.ch/geodata/ch.bl.agi.lidar_2018.dtm/"
+
+// Read (gdal) VRT file to get a list of all tif files.
+def vrt = new groovy.xml.XmlParser().parse("../data/2018/dtm.vrt")
+def tiles = vrt.VRTRasterBand[0].ComplexSource.collect { it ->
+    it.SourceFilename.text().reverse().drop(4).reverse()
+}
+
+tiles.each { tile ->
+    println "Downloading ${tile}"
+    Paths.get(DOWNLOAD_FOLDER, tile + ".tif").toFile().withOutputStream { out ->
+        out << new URL(DOWNLOAD_URL + tile + ".tif").openStream()
+    }
+}
