a31876ac05d82ef9597e1fcf92f2a86b36738961
edigonzales <edi.gonzales@gmail.com>
2021-09-13 15:45:37 +0200

    fix 2018 download

--- app/src/main/groovy/agi/lidar/_2018/Download.groovy
+++ app/src/main/groovy/agi/lidar/_2018/Download.groovy
@@ -2,7 +2,8 @@ package agi.lidar._2018
 
 import java.nio.file.Paths
 
-def DOWNLOAD_FOLDER = "/Volumes/Samsung_T5/geodata/ch.bl.agi.lidar_2018.dtm/"
+def USER_HOME = System.getProperty("user.home");
+def DOWNLOAD_FOLDER = USER_HOME + "/tmp/geodata/ch.bl.agi.lidar_2018.dtm/"
 def DOWNLOAD_URL = "https://geo.so.ch/geodata/ch.bl.agi.lidar_2018.dtm/"
 
 // Read (gdal) VRT file to get a list of all tif files.
